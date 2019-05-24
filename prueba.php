f001861ccbf0963c9e678f673abe4edf4cb6b113
jmg31077 <jhesua2009@gmail.com>
2019-05-24 14:55:17 -0400

    subiendo archivo modificado y archivo de estilo

--- prueba.php
+++ prueba.php
@@ -1,3 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="prueba.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="quote">
+        <div class="box">
+        <?php
 Hola Mundo
 ?>
+        </div>
+    </div>
+</body>
+</html>
